68877a932013aa6cd4b4a57512c3afb5ed31ddf6
redmoondz <rublivsky@gmail.com>
2026-04-02 19:11:33 +0300

    Auto-sync: 2026-04-02 19:11

---
+++ .config/hypr/scripts/80hz.sh
@@ -0,0 +1,4 @@
+cvt 1920 1080 80
+xrandr --newmode "1920x1080_80.00"  236.25  1920 2056 2264 2608  1080 1083 1088 1133 -hsync +vsync
+xrandr --addmode DP-1 "1920x1080_80.00"
+xrandr --output DP-1 --mode "1920x1080_80.00"
